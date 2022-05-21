0f951441ce4b6cd0703547cdf7f44e5d2890be13
Higor Allan <higor.allan21@gmail.com>
2022-05-21 11:12:13 -0300

    forms

--- App.tsx
+++ App.tsx
@@ -1,35 +1,35 @@
 import React from 'react';
 import { Dashboard } from './src/screens/Dashboard';
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components';
 import theme from './src/global/styles/theme';
-import * as SplashScreen from 'expo-splash-screen'
+import * as SplashScreen from 'expo-splash-screen';
 
 import {
-  useFonts,
-  Poppins_400Regular,
-  Poppins_500Medium,
-  Poppins_700Bold
+	useFonts,
+	Poppins_400Regular,
+	Poppins_500Medium,
+	Poppins_700Bold,
 } from '@expo-google-fonts/poppins';
 import { Register } from './src/screens/Register';
 
 export default function App() {
-  SplashScreen.preventAutoHideAsync();
+	SplashScreen.preventAutoHideAsync();
 
-  const [fontsLoaded] = useFonts({
-    Poppins_400Regular,
-    Poppins_500Medium,
-    Poppins_700Bold
-  });
+	const [fontsLoaded] = useFonts({
+		Poppins_400Regular,
+		Poppins_500Medium,
+		Poppins_700Bold,
+	});
 
-  if (!fontsLoaded) {
-    return null
-  }
+	if (!fontsLoaded) {
+		return null;
+	}
 
-  SplashScreen.hideAsync();
+	SplashScreen.hideAsync();
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Register />
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<Register />
+		</ThemeProvider>
+	);
 }
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,34 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '../../components/Forms/Button';
+import { CategorySelect } from '../../components/Forms/CategorySelect';
 import { Input } from '../../components/Forms/Input';
-import { TransactionType } from '../../components/Forms/TransactionType';
-import { 
-  Container,
-  Header,
-  Title,
-  Form,
-  Fields
- } from './styles';
-
+import { TransactionType } from '../../components/Forms/TransactionTypeButton';
+import { TransactionsTypes } from './styles';
+import { Container, Header, Title, Form, Fields } from './styles';
 
 export function Register() {
-  return (
-    <Container>
-      <Header>
-        <Title>Cadastro</Title>
-      </Header>
+	const [transactionType, setTransactionType] = useState('');
 
-     <Form>
-       <Fields>
-          <Input placeholder='Nome' />
-          <Input placeholder='Preco' />
+	function handleTransactionsTypeSelect(type: 'up' | 'down') {
+		setTransactionType(type);
+	}
 
-          <TransactionType type="arrow-up-circle" title="income" />
-       </Fields>
+	return (
+		<Container>
+			<Header>
+				<Title>Cadastro</Title>
+			</Header>
 
-        <Button title="Enviar"/>
-     </Form>
+			<Form>
+				<Fields>
+					<Input placeholder="Nome" />
+					<Input placeholder="Preco" />
 
-    </Container>
-  );
+					<TransactionsTypes>
+						<TransactionType
+							type="up"
+							title="Income"
+							onPress={() => handleTransactionsTypeSelect('up')}
+							isActive={transactionType === 'up'}
+						/>
+						<TransactionType
+							type="down"
+							title="Outcome"
+							onPress={() => handleTransactionsTypeSelect('down')}
+							isActive={transactionType === 'down'}
+						/>
+					</TransactionsTypes>
+
+					<CategorySelect title="Categoria" />
+				</Fields>
+
+				<Button title="Enviar" />
+			</Form>
+		</Container>
+	);
 }
